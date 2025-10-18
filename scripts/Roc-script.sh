27b805e0033745a5355128894a735cb5a2ea97a6
krisxu23 <92844112+krisxu23@users.noreply.github.com>
2025-10-18 08:56:32 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -13,8 +13,39 @@ set -e
 echo "🛠️ 修改默认系统配置..."
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='LiBwrt'/g" package/base-files/files/bin/config_generate
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Kris')/g" \
-  feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Built by Kris')/g" \feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
+# 调整在Argon主题下，概览页面显示/隐藏按钮的样式
+sed -i '/^\.td\.cbi-section-actions {$/,/^}$/ {
+    /^}$/a\
+.cbi-section.fade-in .cbi-title {\
+  position: relative;\
+  min-height: 2.765rem;\
+  display: flex;\
+  align-items: center\
+}\
+.cbi-section.fade-in .cbi-title>div:last-child {\
+  position: absolute;\
+  right: 1rem\
+}\
+.cbi-section.fade-in .cbi-title>div:last-child span {\
+  display: inline-block;\
+  position: relative;\
+  font-size: 0\
+}\
+.cbi-section.fade-in .cbi-title>div:last-child span::after {\
+  content: "\\e90f";\
+  font-family: '\''argon'\'' !important;\
+  font-size: 1.1rem;\
+  display: inline-block;\
+  transition: transform 0.3s ease;\
+  -webkit-font-smoothing: antialiased;\
+  line-height: 1\
+}\
+.cbi-section.fade-in .cbi-title>div:last-child span[data-style='\''inactive'\'']::after {\
+  transform: rotate(90deg);\
+}
+}' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 
 # -------- 移除旧版本冲突包 --------
 echo "🧹 清理旧版或冲突包..."
